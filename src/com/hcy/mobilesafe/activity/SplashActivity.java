181c2694e5c07624c9b04e135752c230c9489ff1
hucongyang <54400092@qq.com>
2015-12-28 14:57:53 +0800

    闪屏页面编写完毕

--- src/com/hcy/mobilesafe/activity/SplashActivity.java
+++ src/com/hcy/mobilesafe/activity/SplashActivity.java
@@ -1,16 +1,40 @@
 package com.hcy.mobilesafe.activity;
 
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.hcy.mobilesafe.R;
 
 public class SplashActivity extends Activity {
+	private TextView tv_version;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		tv_version = (TextView) findViewById(R.id.tv_version);
+		tv_version.setText("版本号:" + getVersion());
+	}
+
+	private String getVersion() {
+		PackageManager packageManager = getPackageManager();
+		try {
+			// 获取包的信息
+			PackageInfo packageInfo = packageManager.getPackageInfo(getPackageName(), 0);
+			int versionCode = packageInfo.versionCode;
+			String versionName = packageInfo.versionName;
+			Log.d("测试1", "versionName=" + versionName + "versionCode" + versionCode);
+			return versionName;
+		} catch (NameNotFoundException e) {
+			// 没有找到包名的时候会走此异常
+			e.printStackTrace();
+		}
+		return "";
 	}
 
 }
