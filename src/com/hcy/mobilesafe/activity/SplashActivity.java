69a67d4d10df1bfd33a7ddfa9a11085d0601dcfc
hucongyang <54400092@qq.com>
2015-12-28 11:47:58 +0800

    第一次提交

---
+++ src/com/hcy/mobilesafe/activity/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.hcy.mobilesafe.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.hcy.mobilesafe.R;
+
+public class SplashActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
